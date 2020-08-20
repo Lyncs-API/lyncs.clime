6dd214b8c3ed7105883bee8fa7cb67cbea6c72da
Simone Bacchio <s.bacchio@gmail.com>
2020-08-20 18:34:37 +0300

    Adding extra_requirements for test

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     data_files=[("test", ["test/conf.unity"])],
     install_requires=["lyncs-cppyy",],
     keywords=["Lyncs", "c-lime", "Lattice QCD",],
+    extras_require={"test": ["pytest", "pytest-cov",]},
     entry_points={"console_scripts": ["lyncs_lime_content = lyncs_clime:reader.main",]},
 )
